160f8f9551fe0a79c91e17a07d5403be25ade8d8
MarsalShyam <shyam12marsal@gmail.com>
2025-02-11 19:16:22 +0530

    gnyan lab is start to creating

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>ram is a good boy</div>
+    </>
+  )
+}
+
+export default App
