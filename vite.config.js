b1c3480926cd444adb67b250c87e344a37579ee7
MarsalShyam <shyam12marsal@gmail.com>
2025-02-15 01:17:49 +0530

    new configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:'/GnyanLab/',
+  // The base path must match your repository name (GnyanLab) and include a trailing slash (/).
   plugins: [react(),
     tailwindcss()],
 })
