72b1b7da9e02c915b36c6bf128c786a638374648
MarsalShyam <shyam12marsal@gmail.com>
2025-02-16 23:29:45 +0530

    side bar is working good and and also the protype is ready

--- src/subjpages/ProtoType.jsx
+++ src/subjpages/ProtoType.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import db from '../assets/db.jpg'
 import { useState, useEffect, useRef } from 'react';
 const ProtoType = () => {
+    const [selectedExp, setSelectedExp] = useState(1);
+
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
     const dropdownRef = useRef(null);
@@ -19,12 +21,16 @@ const ProtoType = () => {
                 setIsSidebarOpen(false);
             }
         };
-        
+
         document.addEventListener('mousedown', handleClickOutside);
         return () => {
             document.removeEventListener('mousedown', handleClickOutside);
         };
     }, []);
+
+    const handleButtonClick = (Exp) => {
+        setSelectedExp(Exp);
+    }
 
     return (
         <div>
@@ -38,7 +44,7 @@ const ProtoType = () => {
                                 type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
                                 <span className="sr-only">Open sidebar</span>
                                 <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
+                                    <path clipRule="evenodd" fillRule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                                 </svg>
                             </button>
                             <a href="#" className="flex ms-2 md:me-24">
@@ -94,73 +100,46 @@ const ProtoType = () => {
 
             <aside
                 ref={sidebarRef}
-
                 id="logo-sidebar"
                 className={`fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"
                     } bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700 sm:translate-x-0`}
                 aria-label="Sidebar">
                 <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
                     <ul className="space-y-2 font-medium">
+                        
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg className="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                                    <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                                    <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                                </svg>
-                                <span className="ms-3">Home</span>
-                            </a>
+                            <div onClick={() => handleButtonClick(1)} className={`px-1 py-2 rounded ${selectedExp === 1 ? 'bg-amber-50 text-black' : 'bg-gray-800'}`}>
+                                1.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum deleniti cupiditate placeat dolorum libero fuga asperiores voluptates aperiam voluptatum?
+                            </div>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-                                    <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-                                </svg>
-                                <span className="flex-1 ms-3 whitespace-nowrap">Exp1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum delectus ab consequuntur illo facere, et corrupti a qui saepe modi ipsam inventore.</span>
-                                <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
-                            </a>
+                            <div onClick={() => handleButtonClick(2)} className={`px-1 py-2 rounded ${selectedExp === 2 ? 'bg-amber-50 text-black' : 'bg-gray-800'}`}>
+                                2.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum deleniti cupiditate placeat
+                            </div></li>
+                        <li>
+                            <div onClick={() => handleButtonClick(3)} className={`px-1 py-2 rounded ${selectedExp === 3 ? 'bg-amber-50 text-black' : 'bg-gray-800'}`}>
+                                3.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum deleniti cupiditate placeat.ram is a good boy.
+                            </div>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
-                                </svg>
-                                <span className="flex-1 ms-3 whitespace-nowrap">Exp2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum delectus ab consequuntur illo facere, et corrupti a qui saepe modi ipsam inventore.</span>
-                                <span className="inline-flex items-center justify-center w-3 h-3 p-3 ms-3 text-sm font-medium text-blue-800 bg-blue-100 rounded-full dark:bg-blue-900 dark:text-blue-300">3</span>
-                            </a>
+                            <div onClick={() => handleButtonClick(4)} className={`px-1 py-2 rounded ${selectedExp === 4 ? 'bg-amber-50 text-black' : 'bg-gray-800'}`}>
+                                4.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum deleniti cupiditate placeat
+                            </div>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                                    <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
-                                </svg>
-                                <span className="flex-1 ms-3 whitespace-nowrap">Exp3. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum delectus ab consequuntur illo facere, et corrupti a qui saepe modi ipsam inventore.</span>
-                            </a>
+                            <div onClick={() => handleButtonClick(5)} className={`px-1 py-2 rounded ${selectedExp === 5 ? 'bg-amber-50 text-black' : 'bg-gray-800'}`}>
+                                5.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum deleniti cupiditate placeat
+                            </div>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-                                    <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
-                                </svg>
-                                <span className="flex-1 ms-3 whitespace-nowrap">Exp4. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum delectus ab consequuntur illo facere, et corrupti a qui saepe modi ipsam inventore.</span>
-                            </a>
+                            <div onClick={() => handleButtonClick(6)} className={`px-1 py-2 rounded ${selectedExp === 6 ? 'bg-amber-50 text-black' : 'bg-gray-800'}`}>
+                                6.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum deleniti cupiditate placeat
+                            </div>
                         </li>
                         <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
-                                </svg>
-                                <span className="flex-1 ms-3 whitespace-nowrap">Exp5. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum delectus ab consequuntur illo facere, et corrupti a qui saepe modi ipsam inventore.</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
-                                    <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
-                                    <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
-                                </svg>
-                                <span className="flex-1 ms-3 whitespace-nowrap">Feedback</span>
-                            </a>
+                            <div conClick={() => handleButtonClick(7)} className={`px-1 py-2 rounded ${selectedExp === 7 ? 'bg-amber-50 text-black' : 'bg-gray-800'}`}>
+                                7.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cum deleniti cupiditate placeat
+                            </div>
                         </li>
                     </ul>
                 </div>
@@ -172,6 +151,49 @@ const ProtoType = () => {
                     <h1>Subject name </h1>
                     <p>Subject Content</p>
                 </div>
+                {selectedExp === 1 && (
+                    <div>
+                        <h1>Exp1 Content</h1>
+                        <p>loreme28</p>
+                    </div>
+                )}
+                {selectedExp === 2 && (
+                    <div>
+                        <h1>Exp2 Content</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui laborum, similique cupiditate exercitationem officiis nemo accusamus, corrupti tenetur et veritatis perferendis, debitis ratione libero iusto aperiam repudiandae eligendi unde! Ad.</p>
+                    </div>
+                )}
+                {selectedExp === 3 && (
+                    <div>
+                        <h1>Exp3 Content</h1>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo quaerat officia voluptatum eos totam minima veniam? Atque dolorum iusto ipsam, asperiores eaque adipisci quas harum suscipit molestias pariatur error voluptas illo?</p>
+                    </div>
+                )}
+                {selectedExp === 4 && (
+                    <div>
+                        <h1>Exp4 Content</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime veritatis repellendus, odio numquam rem, blanditiis culpa perferendis, rerum nihil recusandae vero dolorem nostrum exercitationem mollitia commodi itaque ipsum non neque quidem vitae!</p>
+                    </div>
+                )}
+                {selectedExp === 5 && (
+                    <div>
+                        <h1>Exp5 Content</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum, excepturi voluptatem. Magni, a. Ducimus non nobis quisquam, sapiente, accusamus facere harum voluptas laboriosam vel, minus doloribus tempore fuga nihil molestias perferendis? Quibusdam?</p>
+                    </div>
+                )}
+                {selectedExp === 6 && (
+                    <div>
+                        <h1>Exp6 Content</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse facilis expedita, possimus magnam consectetur incidunt nostrum accusamus obcaecati debitis nisi? Delectus natus officia aperiam, ut nihil quo quibusdam provident saepe dicta quas quisquam nobis optio repellat obcaecati! Placeat nulla inventore maiores provident odio.</p>
+                    </div>
+                )}
+                {selectedExp === 7 && (
+                    <div>
+                        <h1>Exp7 Content</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minus nobis error pariatur tempore. Mollitia tempore facilis provident, molestiae nam debitis, quos voluptas facere dolorum repellendus magnam quaerat veritatis consequuntur saepe quae, ipsa cumque officia.</p>
+                    </div>
+                )}
+
             </div>
 
         </div>
